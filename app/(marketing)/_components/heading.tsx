6138525b69e153f298bfd0f4392767a6c6c689a0
Aymansdk <ayman.sdk123@outlook.com>
2024-08-22 20:51:04 +0100

    feat: Update Heading component with Convex authentication and loading states
    The commit updates the Heading component in the marketing section of the app. It adds Convex authentication functionality using the ConvexClientProvider and SignInButton components from the Convex and Clerk libraries. It also introduces loading states with the Spinner component to provide visual feedback during authentication processes. These changes enhance the user experience by seamlessly integrating Convex authentication and improving the overall usability of the app.

--- app/(marketing)/_components/heading.tsx
+++ app/(marketing)/_components/heading.tsx
@@ -1,9 +1,16 @@
 "use client";
-
+// Local Imports
+import { Spinner } from "@/components/spinner";
 import { Button } from "@/components/ui/button";
 import { ArrowRight } from "lucide-react";
+// Providers Imports
+import { SignInButton } from "@clerk/clerk-react";
+import { useConvexAuth } from "convex/react";
+import Link from "next/link";
 
 export const Heading = () => {
+  const { isAuthenticated, isLoading } = useConvexAuth();
+
   return (
     <div className="max-w-3xl space-y-4">
       <h1 className="text-3xl sm:text-5xl md:text-6xl font-bold">
@@ -14,9 +21,26 @@ export const Heading = () => {
         Notium where organization meets innovation, <br /> empowering you to
         work smarter.
       </h3>
-      <Button>
-        Enter Notium <ArrowRight className="h-4 w-4 ml-2" />
-      </Button>
+      {isLoading && (
+        <div className="w-full flex items-center justify-center">
+          <Spinner size="lg" />
+        </div>
+      )}
+      {isAuthenticated && !isLoading && (
+        <Button asChild>
+          <Link href="/documents">
+            Enter Notium <ArrowRight className="h-4 w-4 ml-2" />
+          </Link>
+        </Button>
+      )}
+      {!isAuthenticated && !isLoading && (
+        <SignInButton mode="modal">
+          <Button>
+            Get Notium free
+            <ArrowRight className="h-4 w-4 ml-2" />
+          </Button>
+        </SignInButton>
+      )}
     </div>
   );
 };
