3f68a46b1f8bdb053ae70275df7f6fe745a3060f
Aymansdk <ayman.sdk123@outlook.com>
2024-08-24 19:37:16 +0100

    feat: Add create child document functionality
    The commit adds functionality to create a child document within the parent document. This allows users to easily create new notes within existing documents. The implementation includes a button to trigger the creation process and displays a toast notification to indicate the success or failure of the operation.

--- app/(main)/_components/item.tsx
+++ app/(main)/_components/item.tsx
@@ -1,10 +1,14 @@
 "use client";
 // shadCN UI components
+import { toast } from "sonner";
 import { Skeleton } from "@/components/ui/skeleton";
-
+// providers imports
+import { api } from "@/convex/_generated/api";
 import { Id } from "@/convex/_generated/dataModel";
 import { cn } from "@/lib/utils";
-import { ChevronDown, ChevronRight, LucideIcon } from "lucide-react";
+import { useMutation } from "convex/react";
+import { ChevronDown, ChevronRight, LucideIcon, Plus } from "lucide-react";
+import { useRouter } from "next/navigation";
 import React from "react";
 
 interface ItemProps {
@@ -32,11 +36,33 @@ export const Item = ({
   onExpand,
   expanded,
 }: ItemProps) => {
+  const router = useRouter();
+  const create = useMutation(api.documents.create);
+
   const handleExpand = (
     event: React.MouseEvent<HTMLDivElement, MouseEvent>
   ) => {
     event.stopPropagation();
     onExpand?.();
+  };
+  // create child document withing the parent document
+  const onCreate = (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+    event.stopPropagation();
+    if (!id) return;
+    const promise = create({ title: "Untitled", parentDocument: id }).then(
+      (documentId) => {
+        if (!expanded) {
+          onExpand?.();
+        }
+        // router.push(`/documents/${documentId}`); // commented out to avoid navigation 404 error
+      }
+    );
+
+    toast.promise(promise, {
+      loading: "Creating new note...",
+      success: "New note created",
+      error: "Failed to create new note",
+    });
   };
 
   const ChevronIcon = expanded ? ChevronDown : ChevronRight;
@@ -57,7 +83,7 @@ export const Item = ({
         <div
           role="button"
           className="h-full rounded-sm hover:bg-slate-300 dark:bg-slate-600 mr-1"
-          onClick={() => {}}
+          onClick={handleExpand}
         >
           <ChevronIcon className="h-4 w-4 shrink-0 text-muted-foreground/50" />
         </div>
@@ -72,6 +98,17 @@ export const Item = ({
         <kbd className="ml-auto pointer-events-none inline-flex h-5 select-none items-center gap-1 rounded border bg-muted px-1.5 font-mono text-[10px] font-medium text-muted-foreground opacity-100">
           <span className="text-sm">ctrl</span>K
         </kbd>
+      )}
+      {!!id && (
+        <div className="ml-auto flex items-center gap-x-2">
+          <div
+            onClick={onCreate}
+            role="button"
+            className="opacity-0 group-hover:opacity-100 h-full ml-auto rounded-sm hover:bg-slate-300 dark:hover:bg-slate-600"
+          >
+            <Plus className="h-4 w-4 text-muted-foreground" />
+          </div>
+        </div>
       )}
     </div>
   );
