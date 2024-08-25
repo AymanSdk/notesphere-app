48e84723e96b6a1575b7466c4bbc26826d6847d5
Aymansdk <ayman.sdk123@outlook.com>
2024-08-25 20:31:10 +0100

    feat: Add DocumentIdPage component
    The commit adds the DocumentIdPage component in the app/(main)/(routes)/documents/[documentId]/page.tsx file. This component represents a page for displaying a specific document based on its ID. It includes a heading element with the text "Document Id Page". This addition enhances the application's routing and document display capabilities, improving the user experience by providing a dedicated page for each document.

---
+++ app/(main)/(routes)/documents/[documentId]/page.tsx
@@ -0,0 +1,9 @@
+const DocumentIdPage = () => {
+  return (
+    <div>
+      <h1>Document Id Page</h1>
+    </div>
+  );
+};
+
+export default DocumentIdPage;
--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -2,6 +2,7 @@
 // Local imports
 import { cn } from "@/lib/utils";
 import { Item } from "./item";
+import { Navbar } from "./navbar";
 import { UserItem } from "./user-item";
 import { TrashBox } from "./trash-box";
 import { useSearch } from "@/hooks/use-search";
@@ -23,15 +24,16 @@ import {
 // Third-party imports
 import { api } from "@/convex/_generated/api";
 import { toast } from "sonner";
-import { usePathname } from "next/navigation";
+import { useParams, usePathname } from "next/navigation";
 import { useMutation } from "convex/react";
 import { DocumentList } from "./document-list";
 import { useMediaQuery } from "usehooks-ts";
 import React, { ElementRef, useEffect, useRef, useState } from "react";
 
 export const Navigation = () => {
-  const search = useSearch();
   const settings = useSettings();
+  const search = useSearch();
+  const params = useParams();
   const pathname = usePathname();
   const isMobile = useMediaQuery("(max-width: 768px)");
   const create = useMutation(api.documents.create);
@@ -185,15 +187,19 @@ export const Navigation = () => {
           isMobile && "left-0 w-full"
         )}
       >
-        <nav className="bg-transparent px-3 py-2 w-full">
-          {isCollapsed && (
-            <MenuIcon
-              onClick={resetWidth}
-              role="button"
-              className="h-6 w-6 text-muted-foreground"
-            />
-          )}
-        </nav>
+        {!!params.documentId ? (
+          <Navbar isCollapsed={isCollapsed} onResetWidth={resetWidth} />
+        ) : (
+          <nav className="bg-transparent px-3 py-2 w-full">
+            {isCollapsed && (
+              <MenuIcon
+                onClick={resetWidth}
+                role="button"
+                className="h-6 w-6 text-muted-foreground"
+              />
+            )}
+          </nav>
+        )}
       </div>
     </>
   );
