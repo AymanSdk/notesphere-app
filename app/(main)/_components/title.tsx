27b211797f57a802e930e0a7385072a6c1208295
Aymansdk <ayman.sdk123@outlook.com>
2024-08-25 22:27:41 +0100

    feat: Add lazy loading for images in DocumentsPage

---
+++ app/(main)/_components/title.tsx
@@ -0,0 +1,72 @@
+"use client";
+
+import { useMutation } from "convex/react";
+import { useState, useRef } from "react";
+
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+
+import { Doc } from "@/convex/_generated/dataModel";
+import { api } from "@/convex/_generated/api";
+
+interface TitleProps {
+  initialData: Doc<"documents">;
+}
+
+export const Title = ({ initialData }: TitleProps) => {
+  const inputRef = useRef<HTMLInputElement>(null);
+  const update = useMutation(api.documents.update);
+
+  const [title, setTitle] = useState(initialData.title || "Untitled");
+  const [isEditing, setIsEditing] = useState(false);
+
+  const enableInput = () => {
+    setTitle(initialData.title);
+    setIsEditing(true);
+    setTimeout(() => {
+      inputRef.current?.focus();
+      inputRef.current?.setSelectionRange(0, inputRef.current.value.length);
+    }, 0);
+  };
+  // Disable input function
+  const disableInput = () => {
+    setIsEditing(false);
+  };
+  // On change function
+  const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setTitle(event.target.value);
+    update({ id: initialData._id, title: event.target.value || "Untitled" });
+  };
+  // On key down function
+  const onKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === "Enter") {
+      disableInput();
+    }
+  };
+
+  return (
+    <div className="flex items-center gap-x-1">
+      {!!initialData && <p>{initialData.icon}</p>}
+      {isEditing ? (
+        <Input
+          ref={inputRef}
+          onClick={enableInput}
+          onBlur={disableInput}
+          onChange={onChange}
+          onKeyDown={onKeyDown}
+          value={title}
+          className="h-7 px-2 focus-visible:ring-transparent"
+        />
+      ) : (
+        <Button
+          onClick={enableInput}
+          variant="ghost"
+          size="sm"
+          className="font-normal h-auto p-1"
+        >
+          <span className="truncate">{initialData?.title}</span>
+        </Button>
+      )}
+    </div>
+  );
+};
